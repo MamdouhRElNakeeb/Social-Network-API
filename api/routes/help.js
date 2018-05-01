58f8e4f441a585b63f58b857167966f0fe53f3e3
Mamdouh El Nakeeb <mamdouh.el.nakeeb@gmail.com>
2018-05-01 15:57:26 +0200

    help function update

--- api/routes/help.js
+++ api/routes/help.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const router = express.Router();
 
-
 router.get('/:admin', (req, res, next) => {
     const admin = req.params.admin;
-    if (admin === 'nh') {
-    
+    if (admin === 'ns') {
         res.status(200).json({
             allResources: [
                 '/api/user'
             ]
         })
 
+    } else {
+        res.status(422).json({
+            message: 'Unauthorized REQUEST'
+        })
     }
-    
 })
-
 
 module.exports = router;

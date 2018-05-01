17ca2884e297fb86879c14612126a932fefe2843
Mamdouh El Nakeeb <mamdouh.el.nakeeb@gmail.com>
2018-05-01 14:37:47 +0200

    help update

--- api/routes/help.js
+++ api/routes/help.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 
-router.get((req, res, next) => {
+router.get('', (req, res, next) => {
     
     res.status(200).json({
         allResources: [
             '/api/user'
         ]
     })
-    
+
 })
 
 
